072dd5aee36fea7e702ba038cfa3cc6f8a5726ac
tylerhanks <teh.hanks@gmail.com>
2020-08-27 21:02:49 -0400

    Implement checks for every piece besides pawns

--- src/cljess/core.cljs
+++ src/cljess/core.cljs
@@ -76,22 +76,51 @@
 (defn get-piece "Get the piece (e.g. :wr) at pos on board"
   [board pos] (cond (vector? pos) (get-in board pos) (keyword? pos) (let [p (board-to-coord pos)] (get-in board p))))
 
+;;utility functions
 (defn on-board? [[y x]] (and (>= x 0) (>= y 0) (< x 8) (< y 8)))
 (defn same-color? [board pos1 pos2] (= (piece-color (get-piece board pos1)) (piece-color (get-piece board pos2))))
 (defn opposite-color? [board pos1 pos2]
   (let [piece1 (get-piece board pos1) piece2 (get-piece board pos2)]
     (or (and (= (piece-color piece1) :w) (= (piece-color piece2) :b)) (and (= (piece-color piece1) :b) (= (piece-color piece2) :w)))))
 
+(defn vector-add "Perform elementwise addition of two equal size vectors" [[x1 y1] [x2 y2]]
+  [(+ x1 x2) (+ y1 y2)])
+
+;;helper functions for finding legal moves
 (defn moves-in-direction "Get the legal moves for a piece at pos on board in direction (e.g. :nw, :up, etc.)"
   [board pos dir]
   (let [dir-vec (case dir :up [-1 0] :down [1 0] :left [0 -1] :right [0 1] :nw [-1 -1] :ne [-1 1] :sw [1 -1] :se [1 1])]
-    (loop [moves #{} next-pos (into [] (map + pos dir-vec))]
+    (loop [moves #{} next-pos (vector-add pos dir-vec)]
       (if-not (on-board? next-pos) moves
-            (if (zero? (get-piece board next-pos)) (recur (conj moves next-pos) (into [] (map + next-pos dir-vec)))
+            (if (zero? (get-piece board next-pos)) (recur (conj moves next-pos) (vector-add next-pos dir-vec))
                 (if (same-color? board pos next-pos) moves (conj moves next-pos)))))))
 
 (defn moves-in-directions [board pos dirs]
   (reduce clojure.set/union (map #(moves-in-direction board pos %) dirs)))
+
+;;helper functions for finding checks
+(defn capture-in-direction [board pos dir]
+  (let [dir-vec (case dir :up [-1 0] :down [1 0] :left [0 -1] :right [0 1] :nw [-1 -1] :ne [-1 1] :sw [1 -1] :se [1 1])]
+    (loop [capture #{} next-pos (vector-add pos dir-vec)]
+      (if-not (on-board? next-pos) capture
+              (if (zero? (get-piece board next-pos)) (recur capture (vector-add next-pos dir-vec))
+                  (if (same-color? board pos next-pos) capture (conj capture (piece-type (get-piece board next-pos)))))))))
+
+(defn captures-in-directions [board pos dirs]
+  (reduce clojure.set/union (map #(capture-in-direction board pos %) dirs)))
+
+(defn knight-captures [board pos]
+  (reduce (fn [res vec]
+            (let [new-pos (vector-add pos vec)]
+              (if (opposite-color? board pos new-pos) (conj res (piece-type (get-piece board new-pos))) res)))
+          #{}
+          [[1 2] [-1 2] [1 -2] [2 1] [-2 1] [2 -1] [-2 -1]]))
+
+(defn check? [board king-pos]
+  (or (contains? (captures-in-directions board king-pos [:up :down :left :right]) :r)
+      (contains? (captures-in-directions board king-pos [:nw :ne :sw :se]) :b)
+      (contains? (captures-in-directions board king-pos [:up :down :left :right :nw :ne :sw :se]) :q)
+      (contains? (knight-captures board king-pos) :n)))
 
 ;;code to find legal moves for a piece at pos on board
 (defmulti legal-moves (fn [{board :board} pos] (piece-type (get-piece board pos))))
